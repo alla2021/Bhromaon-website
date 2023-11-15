994a5c314e97be4eb2f60d59a624d1a8b475aa94
Alla Honcharova <alla7gon@gmail.com>
2023-11-15 18:55:31 +0200

    added gulp, fixed styles

---
+++ src/script/script.js
@@ -0,0 +1,33 @@
+// //scroll
+// document.querySelector('.intro__btn-scroll').addEventListener('click', function() {
+//   scrollToElement('about');
+//});
+
+//function scrollToElement(elementId) {
+//   const element = document.getElementById(elementId);
+//   if (element) {
+//      element.scrollIntoView({ behavior: 'smooth' });
+//   }
+//}
+//$( document ).ready(function() {
+//   $('.js-burger-btn').on('click', function() {
+//       if($('.js-header').hasClass('active')) {
+//           $('.js-header').removeClass('active');
+//       } else {
+//           $('.js-header').addClass('active');
+//       }
+//   })
+//});
+
+
+//$( document ).ready(function() {
+//   $('.js-burger-btn').on('click', function() {
+//       if($('.js-header').hasClass('active')) {
+//           $('.js-header').removeClass('active');
+//       } else {
+//           $('.js-header').addClass('active');
+//       }
+//   })
+//});
+
+
